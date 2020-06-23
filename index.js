0900a7479685a1b3437e30648e1ad1e9b730e73c
carlosziegler <carlos.ziegler@gmail.com>
2020-06-23 17:28:17 +0200

    up to 12 done

--- index.js
+++ index.js
@@ -79,6 +79,81 @@ const limit = 10;
 
 //#6 All the companies that don't include the partners field.
 
+const filter = {
+    'partners': {
+        '$exists': false
+    }
+};
 
+//#7. All the companies that have a null type of value on the `category_code` field.
 
+const filter = {
+    'category_code': {
+        '$eq': null
+    }
+};
 
+//#8 All the companies that have at least 100 employees but less than 1000. Retrieve only the name and number of employees fields.
+
+const filter = {
+    '$and': [
+        {
+            'number_of_employees': {
+                '$gte': 100
+            }
+        }, {
+            'number_of_employees': {
+                '$lte': 1000
+            }
+        }
+    ]
+};
+const projection = {
+    'name': 1,
+    'number_of_employees': 1
+};
+
+//#9 Order all the companies by their IPO price in descending order.
+const filter = {};
+const sort = {
+    'ipo': -1
+};
+
+//#10 Retrieve the ten companies with more employees, order by the number of employees
+
+const filter = {};
+const sort = {
+    'number_of_employees': -1
+};
+const limit = 10;
+
+//#11 All the companies founded in the second semester of the year. Limit your search to 1000 companies.
+
+const filter = {
+    '$and': [
+        {
+            'founded_month': {
+                '$gt': 5
+            }
+        }
+    ]
+};
+const limit = 1000;
+
+//#12 All the companies founded in the second semester of the year. Limit your search to 1000 companies.
+
+const filter = {
+    '$and': [
+        {
+            'founded_year': {
+                '$lte': 2000
+            }
+        }, {
+            'ipo.valuation_amount': {
+                '$gte': 10000000
+            }
+        }
+    ]
+};
+
+  //#
